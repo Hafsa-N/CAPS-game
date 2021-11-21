87b2936210f8756a6ccd94946413345e77ec384e
Hafsa <38572050+Hafsa-N@users.noreply.github.com>
2021-11-21 11:21:27 -0500

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -7,25 +7,25 @@ import ca.roumani.i2c.CountryDB;
 
 public class Game
 {
-    private CountryDB db; //each instance of the class associated with a CountryDB instance, hence this attribute
+    private CountryDB db; 
 
     public Game()
     {
-        this.db = new CountryDB(); //constructor simply inits the attribute
+        this.db = new CountryDB(); 
     }
 
     public String qa()
     {
 
-        List<String> capitals = db.getCapitals(); //get ref to the database's capital city list
-        int  n = capitals.size(); //determine size of list and call it n
-        int index =  (int) (n * Math.random()); //generate random number in [0, n), call it index
+        List<String> capitals = db.getCapitals(); 
+        int  n = capitals.size(); 
+        int index =  (int) (n * Math.random()); //generate random number in [0, n)
 
-        String c = capitals.get(index); //get a random capital city, call it c
-        Map<String, Country> data = db.getData(); //get a ref to the database's data map
-        Country ref = data.get(c); //invoke get(c) on the map to get ref to a country, call it ref
+        String c = capitals.get(index); //get a random capital city
+        Map<String, Country> data = db.getData(); 
+        Country ref = data.get(c); 
 
-        String cName = ref.getName(); //p| get Name of country from ref
+        String cName = ref.getName(); 
 
 
         //generate random question/answer
