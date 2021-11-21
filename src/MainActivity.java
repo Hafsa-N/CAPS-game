badd415ec517ddf11cbe1b01af78a7052887a347
Hafsa <38572050+Hafsa-N@users.noreply.github.com>
2021-11-21 11:23:48 -0500

    Update MainActivity.java

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -10,11 +10,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    private Game game; //to hold Game object instance
-    private String question; //to hold value of prev q
-    private String answer; //to hold value of prev ans
-    private int score = 0; //score is init to 0, value might be updated depending on user's answers
-    private int qNum = 1; //qNum is init to 1, the value will increment as more questions are asked
+    private Game game; 
+    private String question; 
+    private String answer;
+    private int score = 0; 
+    private int qNum = 1; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,22 +22,22 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ask(); //ask a question when app is launched by invoking the ask() method below
+        ask(); 
     }
 
     private void ask()
     {
         if (qNum <= 9)
         {
-            game = new Game();  //a new game object is created whenever ask() is invoked
-            String qa = game.qa();  //get the question/answer string from the qa() method in Game model
+            game = new Game();  
+            String qa = game.qa();  
 
-            String q = qa.substring(0, qa.indexOf("\n") + 1); //this substring of qa gets the question part
-            String ans = qa.substring(qa.indexOf("\n") + 1); //this substring of qa gets the answer part
+            String q = qa.substring(0, qa.indexOf("\n") + 1); 
+            String ans = qa.substring(qa.indexOf("\n") + 1);
 
-            ((TextView) findViewById(R.id.question)).setText(q); //show question in view
-            question = q; //update value of global attribute to hold the current question being asked
-            answer = ans; //similarly..
+            ((TextView) findViewById(R.id.question)).setText(q); 
+            question = q; 
+            answer = ans; 
         }
 
         else
@@ -48,10 +48,10 @@ public class MainActivity extends AppCompatActivity
 
     public void onDone(View v)
     {
-        String prevUserAns = ((EditText) findViewById(R.id.answer)).getText().toString(); //get prev answer from view
+        String prevUserAns = ((EditText) findViewById(R.id.answer)).getText().toString(); 
 
         String showInLog = String.format("%s", "Q#" + qNum + ": " + question + "Your answer: "
-                           + prevUserAns.toUpperCase() + "\nThe correct answer: " + answer); //concat into a string for the log
+                           + prevUserAns.toUpperCase() + "\nThe correct answer: " + answer); 
 
         TextView log = findViewById(R.id.log); //var log for log textview to avoid long concat below
 
@@ -60,22 +60,22 @@ public class MainActivity extends AppCompatActivity
         if (prevUserAns.equalsIgnoreCase(answer))
         {
             score++;
-            ((TextView) findViewById(R.id.score)).setText("SCORE = " + score); //update score in view
+            ((TextView) findViewById(R.id.score)).setText("SCORE = " + score); 
         }
 
-        ((EditText) findViewById(R.id.answer)).setText(""); //erase prev answer from view
+        ((EditText) findViewById(R.id.answer)).setText(""); 
 
-        qNum++; //increment qNum attribute value
-        ((TextView) findViewById(R.id.qNum)).setText("Q# " + qNum); //update qNum in app view
+        qNum++;
+        ((TextView) findViewById(R.id.qNum)).setText("Q# " + qNum); 
 
-        ask(); //ask another question
+        ask(); 
 
        if (qNum == 10)
         {
             ((TextView) findViewById(R.id.qNum)).setText("GAME OVER!");
 
             Button btn = findViewById(R.id.done);
-            btn.setEnabled(false);  //disable done button
+            btn.setEnabled(false);  
           //  finish();
         }
     }
